4a4cc50903511fc68a04aa57624655215911fc93
dmomot <d.momot@pin-up.tech>
2024-03-01 17:08:31 +0200

    Refactor storage implementations

--- internal/adapters/repository/redis.go
+++ internal/adapters/repository/redis.go
@@ -1,14 +1,22 @@
 package examplestore
 
-type Storage interface {}
+import (
+	"github.com/Snork2017/example-srv/internal/domain"
+	"github.com/redis/go-redis/v9"
+)
+
+type Storage interface{}
 
 type StorageRedis struct {
-	inner domain.Storage
-	// ...
+	inner       domain.Storage
+	redisClient *redis.Client
 }
 
-func NewStorageRedis(redisClient) {
-
+func NewStorageRedis(redisClient *redis.Client, inner domain.Storage) *StorageRedis {
+	return &StorageRedis{
+		inner:       inner,
+		redisClient: redisClient,
+	}
 }
 
 // create redis methods which implement Storage interface
